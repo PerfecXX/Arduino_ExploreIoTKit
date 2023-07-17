978e68006f8f78ff1a77906990dc22e6eb4a6e86
Teeraphat Kullanankanjana <watit2013@gmail.com>
2023-07-17 15:30:24 +0700

    Template file for MKR IoT Carrier
    Template file for MKR IoT Carrier

---
+++ example/template.ino
@@ -0,0 +1,27 @@
+/*
+Author      : Teeraphat Kullanankanjana
+Version     : 1.0
+Date        : 17/07/2023
+Description : Template file for using MKR IoT Carrier
+Copyright (C) 2023 Teeraphat Kullanankanjana. All right reserved.
+*/
+
+// Include the Arduino MKR IoT Carrier header file (Library)
+#include <Arduino_MKRIoTCarrier.h>
+
+// Create an instance of the MKRIoTCarrier class
+MKRIoTCarrier carrier;
+
+void setup() {
+  // Initialize the Arduino MKR IoT Carrier
+  carrier.begin();
+  
+  // Initialize the serial communication at 9600 baud rate
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // Your code goes here
+}
+
