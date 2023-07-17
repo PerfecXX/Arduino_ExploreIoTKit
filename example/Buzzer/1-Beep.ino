d8f774d03926a2f09c915d94f31a78eeff436be9
Teeraphat Kullanankanjana <watit2013@gmail.com>
2023-07-17 17:06:26 +0700

    Plays a beep sound on the buzzer.
    Plays a beep sound on the buzzer.

---
+++ example/Buzzer/1-Beep.ino
@@ -0,0 +1,33 @@
+/*
+Author      : Teeraphat Kullanankanjana
+Version     : 1.0
+Date        : 17/07/2023
+Description : Plays a beep sound on the buzzer.
+Copyright (C) 2023 Teeraphat Kullanankanjana. All right reserved.
+*/
+
+// Include the Arduino MKR IoT Carrier header file (Library)
+#include <Arduino_MKRIoTCarrier.h>
+
+// Create an instance of the MKRIoTCarrier class
+MKRIoTCarrier carrier;
+
+// Freqency and duration(ms)
+int freqency = 800;
+int duration_ms = 300;
+
+void setup() {
+  // Initialize the Arduino MKR IoT Carrier
+  carrier.begin();
+  
+  // Initialize the serial communication at 9600 baud rate
+  Serial.begin(9600);
+
+  // Play a beep sound on the buzzer
+  carrier.Buzzer.beep(freqency, duration_ms);
+}
+
+
+void loop() {
+  // Your code goes here
+}
