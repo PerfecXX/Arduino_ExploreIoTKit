b7ca720210f38444a41ed00749c8e895fa4259be
Teeraphat Kullanankanjana <watit2013@gmail.com>
2023-07-22 11:14:45 +0700

    Read Gyroscope
    Read Gyroscope

---
+++ example/IMU/2-ReadGyroscope.ino
@@ -0,0 +1,40 @@
+/*
+  Author      : Teeraphat Kullanankanjana
+  Version     : 1.0
+  Date        : 22/07/2023
+  Description : Read Gyroscope Data from IMU Module
+  Copyright (C) 2023 Teeraphat Kullanankanjana. All rights reserved.
+*/
+
+// Include the Arduino MKR IoT Carrier header file (Library)
+#include <Arduino_MKRIoTCarrier.h>
+
+// Create an instance of the MKRIoTCarrier class
+MKRIoTCarrier carrier;
+
+// Variables to store gyroscope data along the x, y, and z axes
+float gX, gY, gZ;
+
+void setup() {
+  // Initialize the Arduino MKR IoT Carrier
+  carrier.begin();
+
+  // Initialize the serial communication at 9600 baud rate
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Check if the IMU module has gyroscope data available
+  if (carrier.IMUmodule.gyroscopeAvailable()) {
+    // Read the gyroscope data along the x, y, and z axes
+    carrier.IMUmodule.readGyroscope(gX, gY, gZ);
+  }
+
+  // Print the gyroscope data to the serial monitor
+  Serial.print("gX: ");
+  Serial.print(gX);
+  Serial.print(" gY: ");
+  Serial.print(gY);
+  Serial.print(" gZ: ");
+  Serial.println(gZ);
+}
